726f466e012c631fa44b8301a25f77b6869dbb24
Dan Jones <dan.j.jones88@gmail.com>
2021-06-06 09:48:13 +0100

    Placeholder "Convert" endpoint

---
+++ Converter/Web/Controllers/ConvertController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConvertController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(double value, int fromUnitId, int toUnitId)
+        {
+            return Ok(1.1);
+        }
+    }
+}
